1e401f4bef7c71bf2ed5347bb3b086c530b1b836
luismiguel0529 <l.miguel0529@hotmail.com>
2021-05-12 00:50:00 -0500

    add proyect reactive

---
+++ src/main/java/com/java/reactive/utils/AppUtils.java
@@ -0,0 +1,20 @@
+package com.java.reactive.utils;
+
+import com.java.reactive.dto.ProductDto;
+import com.java.reactive.entity.Product;
+import org.springframework.beans.BeanUtils;
+
+public class AppUtils {
+
+    public static ProductDto entityToDto(Product product){
+        ProductDto productDto = new ProductDto();
+        BeanUtils.copyProperties(product,productDto);
+        return productDto;
+    }
+
+    public static Product dtoToEntity(ProductDto productDto){
+        Product product = new Product();
+        BeanUtils.copyProperties(productDto,product);
+        return product;
+    }
+}
